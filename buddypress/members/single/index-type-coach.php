f4e0895e221594d675bcfcbe3e893eaeb423c3d1
imbdev <moose@inspiredmommyboss.com>
2020-10-08 08:58:50 +0800

    Coach Profile ongoing -- structure - 8 oct 2020

--- buddypress/members/single/index-type-coach.php
+++ buddypress/members/single/index-type-coach.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * BuddyPress - Members Home
+ * ASM - ATHLETE PROFILE
  *
  * @since   1.0.0
  * @version 3.0.0
@@ -9,70 +9,161 @@ get_header();
 ?>
 <div id="buddypress" class="buddypress-wrap coach bp-dir-hori-nav">
   <!-- FONT AWESOME 5 PRO -->
-  <script src="https://kit.fontawesome.com/c8084101a0.js" crossorigin="anonymous"></script>
+  <!-- <script src="https://kit.fontawesome.com/c8084101a0.js" crossorigin="anonymous"></script> -->
+  <?php
+  
+    $user_id = bp_displayed_user_id();
+    $current_user = wp_get_current_user();
+    $current_user_id = $current_user->ID;
+    $user_id_for_ACF = "user_" . $user_id;
+    $committed_status = get_field( 'athlete_availability', $user_id_for_ACF );
+    $committed_university_name = get_field( 'university_name', $user_id_for_ACF );
+    $committed_university_logo = get_field( 'university_logo', $user_id_for_ACF );
+    $verification = get_field( 'athlete_verification', $user_id_for_ACF );
+    $sport = xprofile_get_field_data( 49, $user_id, $multi_format = 'array' );
 
-  <div class="top-page-navbar-coach">
+    if ( ! $verification || $verification == 'no' ) {
+
+      $v_result = '
+      <li class="d-inline meta-icon">
+        <i class="fas fa-times-circle"></i>
+      </li>
+      <li class="d-inline meta-text">
+        <span class="text">Unverified Profile</span>
+      </li>
+      ';
+
+    } else {
+
+      $v_result = '
+      <li class="d-inline meta-icon">
+        <i class="fas fa-check-circle"></i>
+      </li>
+      <li class="d-inline meta-text">
+        <span class="text">Verified Profile</span>
+      </li>
+      ';
+
+    }
+
+  ?>
+
+  <div class="top-page-navbar">
 
     <section class="top-page-menu clearfix">
+
       <h3 class="menu-title float-left">Profile</h3>
       <?php if (is_user_logged_in() && ($user_id == $current_user_id)) : ?>
 
-      <h3 id="edit-coach-profile-button" class="menu-title float-right">Edit Profile</h3>
+      <!-- <h3 id="edit-athlete-profile-button" class="menu-title float-right">Edit Profile</h3> -->
 
       <?php endif; ?>
+
     </section>
   </div>
-  <div class="top-filter-navbar-coach">
+  <div class="top-filter-navbar">
 
     <section class="top-page-menu clearfix">
       <h3 class="menu-title float-left">Filter</h3>
+      <h3 id="edit-athlete-profile-button" class="menu-title float-right">
+        Edit Profile &nbsp;
+        <i class="fas fa-pen-square"></i>
+      </h3>
+
       <ul class="list-inline menu-box">
         <a href="#top-video-section">
-
-          <li class="list-inline-item"><img class="img-icon"
-              src="/wp-content/uploads/Screen-Shot-2020-03-30-at-11.29.14-AM-e1585539321304.png" alt="">
+          <li class="list-inline-item"><img class="img-icon" src="/wp-content/uploads/video-icon.png" alt="">
             <span class="btn-text">Video</span>
           </li>
-
         </a>
         <a href="#about-section">
           <!-- <li class="list-inline-item"><i class="fad fa-user-circle"></i>Bio</li> -->
-          <li class="list-inline-item"><img class="img-icon" src="/wp-content/uploads/bio-icon.png" alt="">
+          <li class="list-inline-item"><img class="img-icon" src="/wp-content/uploads/bio-icon-2.png" alt="">
             <span class="btn-text">Bio</span>
           </li>
         </a>
-
-
+        <a href="#sports-stats-section">
+          <!-- <li class="list-inline-item"><i class="fad fa-trophy-alt"></i></i>Sports Stats</li> -->
+          <li class="list-inline-item"><img class="img-icon" src="/wp-content/uploads/awards-icon.png" alt="">
+            <span class="btn-text">Sports Info</span>
+          </li>
+        </a>
         <a href="#academics-section">
           <!-- <li class="list-inline-item"><i class="fad fa-file-certificate"></i>Academics</li> -->
-          <li class="list-inline-item"><img class="img-icon"
-              src="/wp-content/uploads/Screen-Shot-2020-03-30-at-11.30.01-AM.png" alt="">
+          <li class="list-inline-item"><img class="img-icon" src="/wp-content/uploads/academics-icon-1.png" alt="">
             <span class="btn-text">Academics</span>
           </li>
         </a>
         <a href="#instagram-section">
           <!-- <li class="list-inline-item"><i class="fab fa-instagram"></i>Instagram</li> -->
-          <li class="list-inline-item"><img class="img-icon" style="padding-top: .2rem;"
-              src="/wp-content/uploads/pics-icon.png" alt="">
+          <li class="list-inline-item"><img class="img-icon" style="padding-top: .1rem;"
+              src="/wp-content/uploads/images-icon.png" alt="">
             <span class="btn-text">Instagram</span>
           </li>
         </a>
-
+        <a href="#interview-section">
+          <!-- <li class="list-inline-item"><i class="fad fa-comments"></i>Interviews</li> -->
+          <li class="list-inline-item"><img class="img-icon" src="/wp-content/uploads/interview-icon-2.png" alt="">
+            <span class="btn-text">Interviews</span>
+          </li>
+        </a>
       </ul>
 
     </section>
   </div>
+
   <div class="bp-wrap">
+    <section id="large-profile-header">
+
+      <div class="container-fluid">
+        <div class="row">
+
+          <!-- ATHLETE AVATAR IMAGE -->
+          <article class="col-4 col-md-3">
+            <div>
+              <!-- <a href="<?php //bp_displayed_user_link(); ?>"> -->
+
+              <div class="athlete-img-box text-center">
+
+                <?php bp_displayed_user_avatar( 'type=full' ); ?>
+
+              </div>
+
+              <!-- </a> -->
+            </div><!-- #item-header-avatar -->
+          </article>
+
+          <!-- ATHLETE NAME - VIEW COUNT - VARIFICATION -->
+          <article class="col-7 col-md-7">
+
+            <div class="title-box">
+              <h1 class="athlete-title">
+
+                <?php echo bp_core_get_user_displayname( $user_id, true ); ?>
+
+                <small>(<?php echo $sport; ?>)</small>
+
+              </h1>
+            </div>
+
+          </article>
+
+        </div>
+      </div>
+
+    </section> <!-- END OF id="large-profile-header" -->
 
     <div id="item-body" class="item-body row">
       <!-- LEFT COLUMN START -->
-      <article class="left-col col-sm-12 col-md-12 col-lg-4 col-xl-3">
+      <article class="left-col col-sm-12 col-md-12 col-lg-5 col-xl-3">
 
         <!-- LEFT-INFO-BOX SECTION START -->
 
         <?php 
-      get_template_part('/buddypress/members/single/_moose-template-parts/coach-profile-left-info-box');
-      ?>
+      
+          get_template_part('/buddypress/members/single/_moose-template-parts/athlete-profile-left-info-box');
+      
+        ?>
 
         <!-- LEFT-INFO-BOX SECTION END -->
 
@@ -80,13 +171,7 @@ get_header();
       <!-- LEFT COLUMN END -->
 
       <!-- CENTER COLUMN START -->
-      <article class="col-sm-12 col-md-12 col-lg-8 col-xl-6">
-
-        <?php
-          $user_id = bp_displayed_user_id();
-          $current_user = wp_get_current_user();
-          $current_user_id = $current_user->ID;
-        ?>
+      <article class="col-sm-12 col-md-12 col-lg-7 col-xl-9">
 
         <?php if (is_user_logged_in() && ($user_id == $current_user_id)) : ?>
         <!-- SETTING UP EMAIL INBOX -->
@@ -97,7 +182,7 @@ get_header();
         <!-- THE EDIT MENU START -->
         <?php if (is_user_logged_in() && ($user_id == $current_user_id)) : ?>
         <!-- FOLLOWING IS USED FOR THE PROFILE EDIT FUNCTION ONLY -->
-        <section id="coach-profile-edit-box" class="">
+        <section id="athlete-profile-edit-box" class="">
           <?php bp_nouveau_member_template_part(); ?>
         </section>
         <?php endif; ?>
@@ -128,7 +213,7 @@ get_header();
         <!-- SPORTS STATS SECTION STARTS -->
 
         <?php 
-      // get_template_part('/buddypress/members/single/_moose-template-parts/athlete-profile-sports-stats');
+      get_template_part('/buddypress/members/single/_moose-template-parts/athlete-profile-sports-stats');
       ?>
 
         <!-- SPORTS STATS SECTION END -->
@@ -136,7 +221,7 @@ get_header();
         <!-- ACADEMICS SECTION START -->
 
         <?php 
-      get_template_part('/buddypress/members/single/_moose-template-parts/coach-profile-university');
+      get_template_part('/buddypress/members/single/_moose-template-parts/athlete-profile-academics');
       ?>
 
         <!-- ACADEMICS SECTION END -->
@@ -153,7 +238,7 @@ get_header();
 
 
         <?php 
-      // get_template_part('/buddypress/members/single/_moose-template-parts/athlete-profile-interview');
+      get_template_part('/buddypress/members/single/_moose-template-parts/athlete-profile-interview');
       ?>
 
 
@@ -166,50 +251,6 @@ get_header();
       <!-- CENTER COLUMN END -->
 
       <!-- RIGHT COLUMN - SIDEBAR COLUMN STARTS -->
-
-      <article class="col-sm-12 col-md-12 col-lg-12 col-xl-3">
-
-        <!-- ATHLETE PROFILE RIGHT EMAIL BUTTON SET START -->
-
-        <!-- MESSAGE BUTTON -->
-        <?php // if (is_user_logged_in() && ($user_id == $current_user_id)) : ?>
-
-        <section id="athlete-right-email-button">
-          <a href="/coach-email-inbox/" class="clearfix">
-            <div class="message-button">
-              <img src="/wp-content/uploads/email-btn-icon.png" alt="" class="float-left btn-image">
-              <div class="btn-text-holder">
-                <h5 class="btn-text">MESSAGE ATHLETE</h5>
-              </div>
-            </div>
-          </a>
-
-        </section>
-        <?php // endif; ?>
-
-
-        <!-- SEARCH ATHLETE BUTTON -->
-
-        <section id="athlete-right-search-athlete-button">
-          <a href="/members/type/athletes/" class="clearfix">
-            <div class="message-button">
-              <img src="/wp-content/uploads/search-athlete-icon.png" alt="" class="float-left btn-image">
-              <div class="btn-text-holder">
-                <h5 class="btn-text">SEARCH ATHLETE</h5>
-              </div>
-            </div>
-          </a>
-
-        </section>
-
-        <!-- ATHLETE PROFILE RIGHT EMAIL BUTTON SET END -->
-
-        <!-- THE SIDEBAR -->
-        <div class="sidebar-area mt-5">
-          <?php get_sidebar(); ?>
-        </div>
-
-      </article>
 
     </div><!-- #item-body -->
 
@@ -224,7 +265,7 @@ get_header();
   var adjustAnchor = function() {
 
     var $anchor = $(':target'),
-      fixedElementHeight = 230;
+      fixedElementHeight = 440;
 
     if ($anchor.length > 0) {
 
